acb37fd976f3cc4d8885c4015d371146cef06b4c
ivanchetvergov <ivar5165885@gmail.com>
2025-09-04 21:22:57 +0300

    fix .gitignore

--- oop/lab1/func.cpp
+++ oop/lab1/func.cpp
@@ -7,25 +7,25 @@ void f1() {
 		double y;
 		if (x <= 1 && x >= 0) y = 0;
 		else if (x <= 2) y = 2 * x - 2; 
-		else if (x <= 3) y = 2 * x + 6;
+		else if (x <= 3) y = -2 * x + 6;
 		else y = 0; 
 		std::cout << "x = " << x << ", y = " << y << std::endl;
 	}
 }
 
 void f2() {
-    for (double x = 0.; x <= 4.; x += 0.) {
+    for (double x = 0.; x <= 4.; x += 0.1) {
         double y = (x < 1.) ? 0. : ((x <= 2.) ? 2. * x - 2. : ((x <= 3.) ? -2. * x + 6. : 0.));
         std::cout << "x = " << x << ", y = " << y << std::endl;
     }
 }
 
 void f3() {
-    std::cout << "\nВведите символы (a, b, c, q или Esc):\n";
+    std::cout << "\nвведите символы (a, b, c, q или Esc):\n";
     int count = 0;
     for (;;) { // inf loop
         char c;
-        std::cin >> c;
+        std::cin >> c; //getch
         count++;
 
         switch (c) {
@@ -53,11 +53,11 @@ void f3() {
 }
 
 void f4() {
-    unsigned short flags = 0xaabb; // 10101010 10111011
+     short flags = 0xaabb; // 10101010 10111011
     std::cout << "was: " << std::hex << flags << std::endl;
     
     // достаем байты путем сдвига
-    unsigned short high_byte = (flags >> 8);  
+    unsigned short high_byte = (flags >> 8). & (0x00ff);  
     unsigned short low_byte = (flags << 8); 
 
     unsigned short swapped_flags = high_byte | low_byte; // "складываем" результат
@@ -73,8 +73,10 @@ void f5() {
     byte |= 0b00001000;
     std::cout << "третий бит: " << std::hex << (int)byte << std::endl; //  0x2e (0010 1110)
 
-    byte ^= 0b00000011; // XOR 
-    std::cout << "инверт 2 мл бита: " << std::hex << (int)byte << std::endl; // 0x2d (0010 1101)
+     bbyte ^= 0b00000011; // XOR 
+     bbyte ^= 0b00000011; // XOR 
+
+     std::cout << "инверт 2 мл бита: " << std::hex << (int)byte << std::endl; // 0x2d (0010 1101)
     
     byte &= 0b11110000; // "умножаем хвост"
     std::cout << "занулили 4 мл бита: " << std::hex << (int)byte << std::endl; // Результат: 0x20 (0010 0000)
