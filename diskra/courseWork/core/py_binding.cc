02ef5dea079e5de9d442f4dd3c0c9458dd0a5c38
ivanchetvergov <ivar5165885@gmail.com>
2025-09-25 22:13:44 +0300

    connect c++ lib to python

---
+++ diskra/courseWork/core/py_binding.cc
@@ -0,0 +1,21 @@
+#include <pybind11/pybind11.h>
+#include "FiniteField.h"
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(finite_field_module, m) {
+    py::class_<FiniteFieldRules>(m, "FiniteFieldRules")
+        .def(py::init<const std::string&, const std::string&>())
+        .def("getZeroElement", &FiniteFieldRules::getZeroElement)
+        .def("getOneElement", &FiniteFieldRules::getOneElement)
+        .def("getCharValue", &FiniteFieldRules::getCharValue)
+        .def("getValueChar", &FiniteFieldRules::getValueChar)
+        .def("getSize", &FiniteFieldRules::getSize);
+
+    py::class_<FiniteField>(m, "FiniteField")
+        .def(py::init<const FiniteFieldRules&>())
+        .def("add", &FiniteField::add)
+        .def("subtract", &FiniteField::subtract)
+        .def("multiply", &FiniteField::multiply)
+        .def("divide", &FiniteField::divide);
+}
