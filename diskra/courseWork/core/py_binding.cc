b02af89135ef73e341d2b257affe4a53a1b45f31
ivanchetvergov <ivar5165885@gmail.com>
2025-10-21 20:49:00 +0300

    fix: pybind file

--- diskra/courseWork/core/py_binding.cc
+++ diskra/courseWork/core/py_binding.cc
@@ -13,23 +13,10 @@ PYBIND11_MODULE(finite_field_module, m) {
         .def("getSize", &FiniteFieldRules::getSize);
 
     py::class_<FiniteField>(m, "FiniteField")
-    .def(py::init<const FiniteFieldRules&>())
-    .def("add", [](const FiniteField &f, const std::string &a, const std::string &b) {
-        if (a.size() != 1 || b.size() != 1) throw std::runtime_error("expected single-character string");
-        return f.add(a[0], b[0]);
-    })
-    .def("subtract", [](const FiniteField &f, const std::string &a, const std::string &b) {
-        if (a.size() != 1 || b.size() != 1) throw std::runtime_error("expected single-character string");
-        return f.subtract(a[0], b[0]);
-    })
-    .def("multiply", [](const FiniteField &f, const std::string &a, const std::string &b) {
-        if (a.size() != 1 || b.size() != 1) throw std::runtime_error("expected single-character string");
-        return f.multiply(a[0], b[0]);
-    })
-    .def("divide", [](const FiniteField &f, const std::string &a, const std::string &b) {
-        if (a.size() != 1 || b.size() != 1) throw std::runtime_error("expected single-character string");
-        return f.divide(a[0], b[0]);
-    })
-    .def("getRules", &FiniteField::getRules, py::return_value_policy::reference_internal);
-
+        .def(py::init<const FiniteFieldRules&>())
+        .def("add", &FiniteField::add)
+        .def("subtract", &FiniteField::subtract)
+        .def("multiply", &FiniteField::multiply)
+        .def("divide", &FiniteField::divide)
+        .def("getRules", &FiniteField::getRules, py::return_value_policy::reference_internal);
 }
